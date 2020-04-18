8dfd7e1ea352a367799befda20a8b3d716a597f0
roman.zubov <zuroin@gmail.com>
2020-04-18 20:31:49 +0300

    - fixed middleware for case when no env in the request

--- api/middlewares/setLanguage.js
+++ api/middlewares/setLanguage.js
@@ -1,5 +1,5 @@
 module.exports = function post(req, res, next) {
-    const {env} = req.body;
+    const env = req.body ? req.body.env : {};
     const lang = env.language ? env.language.toLowerCase() : 'en';
     res.setLocale(lang);
     console.log('lang set:', lang);
